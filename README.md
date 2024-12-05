68ba4da240b2c4bd286df7a222ce2a657ce93348
Rohin <git@r0h.in>
2024-12-05 14:00:23 -0500

    Readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# lens
-LENS CRYPTOCURRENCY WORLD WIDE WEB 3.0 PROTPOCOAL
+# Lens
+
+```
+This repository contains the source code for Lens, including a React Native front-end client, Solidity smart contract, and our Lenscrypt 2.0 encryption protocol.
+```
+
+Read more at [our website](https://lens.red.r0h.in).
+
+![](2024-12-05-13-59-40.png)
+
+![](2024-12-05-13-59-54.png)
--- docs/src/app/page.tsx
+++ docs/src/app/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div className="z-10 w-full h-full max-w-5xl flex flex-col items-center justify-center font-mono text-sm">
-
         <Image src={logo} alt="" width={128} />
         <h1 className="text-2xl font-bold font-mono text-center mb-8">
           Lens is a better way to prove trust.
@@ -17,17 +16,17 @@ export default function Home() {
               Demo
             </button>
           </Link>
-          <Link href={"https://projectlens.xyz/paper.pdf"}>
+          <Link href={"https://lens.red.r0h.in/paper.pdf"}>
             <button className="font-bold py-3 px-6 m-2 rounded-xl border border-[#5371FF] text-[#a3a3a3]">
               White Paper
             </button>
           </Link>
-          <Link href={"https://projectlens.xyz/deck.pdf"}>
+          <Link href={"https://lens.red.r0h.in/deck.pdf"}>
             <button className="font-bold py-3 px-6 m-2 rounded-xl border border-[#5371FF] text-[#a3a3a3]">
               Deck
             </button>
           </Link>
-          <Link href={"mailto:rohin@projectlens.xyz"}>
+          <Link href={"mailto:rohin@jaimy.ai,jeff@jaimy.ai"}>
             <button className="font-bold py-3 px-6 m-2 rounded-xl border border-[#404040] text-[#a3a3a3]">
               Contact
             </button>
